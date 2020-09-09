4ad89f7125b1becbed2c41b4460effd6ebaa8b3a
ndgndg91 <ndgndg91@gmail.com>
2020-09-09 22:18:59 +0900

    category, product entity

---
+++ src/main/java/com/ndgndg91/ecommerce/entity/Product.java
@@ -0,0 +1,51 @@
+package com.ndgndg91.ecommerce.entity;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private ProductCategory category;
+
+    @Column(name = "sku")
+    private String sku;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description1")
+    private String description;
+
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+
+    @Column(name = "image_url")
+    private  String imageUrl;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @Column(name = "unit_in_stock1")
+    private int unitsInStock;
+
+    @Column(name = "date_created")
+    @CreationTimestamp
+    private LocalDate dataCreated;
+
+    @Column(name = "last_updated")
+    @UpdateTimestamp
+    private LocalDate lastUpdated;
+}
