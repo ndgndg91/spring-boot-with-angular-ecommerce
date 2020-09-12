5229c5e941347a203b3d53f9e3f8a481b932c637
ndgndg91 <ndgndg91@gmail.com>
2020-09-12 21:59:40 +0900

    findByCategoryId end point

--- src/main/java/com/ndgndg91/ecommerce/controller/ProductController.java
+++ src/main/java/com/ndgndg91/ecommerce/controller/ProductController.java
@@ -2,16 +2,24 @@ package com.ndgndg91.ecommerce.controller;
 
 import com.ndgndg91.ecommerce.entity.Product;
 import com.ndgndg91.ecommerce.repository.ProductRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @CrossOrigin("http://localhost:4200")
 public class ProductController {
+
+    private static final Logger log = LoggerFactory.getLogger(ProductController.class);
 
     private final ProductRepository productRepository;
 
@@ -20,14 +28,26 @@ public class ProductController {
     }
 
     @GetMapping("/api/products")
-    public ResponseEntity<Products> products(){
-        return ResponseEntity.ok(new Products(productRepository.findAll()));
+    public ResponseEntity<GetProductResponse> products(){
+        log.info("call API");
+        return ResponseEntity.ok(new GetProductResponse(productRepository.findAll()));
     }
 
-    private class Products{
+    @GetMapping("/api/products/search/findByCategoryId")
+    public ResponseEntity<GetProductResponse> productByCategoryId(
+            @RequestParam long id,
+            Pageable pageable
+    ){
+        log.info("call Category API");
+        Page<Product> byCategoryId = productRepository.findByCategoryId(id, pageable);
+        List<Product> collect = byCategoryId.stream().collect(Collectors.toList());
+        return ResponseEntity.ok(new GetProductResponse(collect));
+    }
+
+    private static class GetProductResponse {
         private final List<Product> products;
 
-        Products(List<Product> products) {
+        GetProductResponse(List<Product> products) {
             this.products = products;
         }
 
--- src/main/java/com/ndgndg91/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/ndgndg91/ecommerce/repository/ProductRepository.java
@@ -1,7 +1,12 @@
 package com.ndgndg91.ecommerce.repository;
 
 import com.ndgndg91.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 }
